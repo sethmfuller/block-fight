f236106f292ce41529b8d5e992c24447d2e21798
ashcl <christopher.lee.ash@gmail.com>
2018-11-28 14:01:54 -0500

    merge from initialSetup

--- src/BlockFight.py
+++ src/BlockFight.py
@@ -1,12 +1,34 @@
-import math
+# Author: Seth M. Fuller
+# File: BlockFight.py
+# Date: 11/27/2018
+
+__docformat__ = "reStructuredText"
+
+import sys, random
+import os
+
+description = """
+---- Block File ----
+A Cool Game
+"""
+
+is_interactive = False
+display_flags = 0
+
+### We must set OS env before the pygame imports..
 import pygame
-import pymunk
+from pygame.locals import *
+from pygame.color import *
+
+display_size = (600, 600)
+
+import pymunk as pm
 from pymunk import Vec2d
+import math
 
-
-class PymunkSprite(pymunk.Body):
+class PymunkSprite(pm.Body):
     def __init__(self, image, mass, moment):
-        pymunk.Body.__init__(self, mass, moment)
+        pm.Body.__init__(self, mass, moment)
         self.imageMaster = pygame.image.load(image).convert()
         self.imageMaster.set_colorkey(self.imageMaster.get_at((0, 0)))
 
@@ -27,3 +49,104 @@ class PymunkSprite(pymunk.Body):
         p = p - offset
 
         screen.blit(rotated_logo_img, p)
+
+def drawcircle(image, colour, origin, radius, width=0):
+    if width == 0:
+        pygame.draw.circle(image, colour, origin, int(radius))
+    else:
+        if radius > 65534 / 5:
+            radius = 65534 / 5
+        circle = pygame.Surface([radius * 2 + width, radius * 2 + width]).convert_alpha()
+        circle.fill([0, 0, 0, 0])
+        pygame.draw.circle(circle, colour, [circle.get_width() / 2, circle.get_height() / 2], radius + (width / 2))
+        if int(radius - (width / 2)) > 0:
+            pygame.draw.circle(circle, [0, 0, 0, 0], [circle.get_width() / 2, circle.get_height() / 2],
+                               abs(int(radius - (width / 2))))
+        image.blit(circle, [origin[0] - (circle.get_width() / 2), origin[1] - (circle.get_height() / 2)])
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(display_size, display_flags)
+    width, height = screen.get_size()
+
+    def to_pygame(p):
+        """Small hack to convert pymunk to pygame coordinates"""
+        return int(p.x), int(-p.y + height)
+
+    def from_pygame(p):
+        return to_pygame(p)
+
+    clock = pygame.time.Clock()
+    running = True
+    font = pygame.font.Font(None, 16)
+
+    ### Physics stuff
+    space = pm.Space()
+    space.gravity = (0.0, -1900.0)
+    space.damping = 0.999  # to prevent it from blowing up.
+    mouse_body = pm.Body(body_type=pm.Body.KINEMATIC)
+
+    # Create bodies
+    bodies = []
+    offset_y = height / 2
+    mass = 10
+    radius = 25
+    moment = pm.moment_for_circle(mass, 0, radius, (0, 0))
+
+    # Create body
+    body = pm.Body(mass, moment)
+    body.position = (width/2, -125 + offset_y)
+    body.start_position = Vec2d(body.position)
+
+    # Create shape
+    shape = pm.Circle(body, radius)
+    shape.elasticity = 0.9999999
+    shape.color = THECOLORS["green"]
+
+    # Create joint
+    pj = pm.PinJoint(space.static_body, body, (width/2, 125 + offset_y), (0, 0))
+
+    # Add objects to space
+    space.add(body, shape, pj)
+
+    bodies.append(body)
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+
+        mpos = pygame.mouse.get_pos()
+        p = from_pygame(Vec2d(mpos))
+        mouse_body.position = p
+
+        ### Clear screen
+        screen.fill(THECOLORS["white"])
+
+        ### Draw stuff
+        for c in space.constraints:
+            pv1 = c.a.position + c.anchor_a
+            pv2 = c.b.position + c.anchor_b
+            p1 = to_pygame(pv1)
+            p2 = to_pygame(pv2)
+            pygame.draw.aalines(screen, THECOLORS["lightgray"], False, [p1, p2])
+
+        for box in space.shapes:
+            p = to_pygame(box.body.position)
+            drawcircle(screen, box.color, p, int(box.radius), 0)
+            # pygame.draw.circle(screen, ball.color, p, int(ball.radius), 0)
+
+        ### Update physics
+        fps = 50
+        iterations = 25
+        dt = 1.0 / float(fps) / float(iterations)
+        for x in range(iterations):  # 10 iterations to get a more stable simulation
+            space.step(dt)
+
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
